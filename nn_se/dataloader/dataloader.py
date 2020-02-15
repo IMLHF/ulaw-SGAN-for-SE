0882a412f5ecd559f273fd5c81b468634a74718f
IMLHF <red_wind@foxmail.com>
2020-02-15 21:35:00 +0800

    do not use tfrecords

--- nn_se/dataloader/dataloader.py
+++ nn_se/dataloader/dataloader.py
@@ -14,48 +14,48 @@ class DataSetsOutputs(
   pass
 
 
-def parse_func(record_proto):
-  wav_len = int(PARAM.sampling_rate*PARAM.train_val_wav_seconds)
-  features = {
-      'clean': tf.io.FixedLenFeature([wav_len], tf.float32),
-      'mixed': tf.io.FixedLenFeature([wav_len], tf.float32)
-  }
-  record = tf.io.parse_single_example(record_proto, features=features)
-  return record['clean'], record['mixed']
+# def parse_func(record_proto):
+#   wav_len = int(PARAM.sampling_rate*PARAM.train_val_wav_seconds)
+#   features = {
+#       'clean': tf.io.FixedLenFeature([wav_len], tf.float32),
+#       'mixed': tf.io.FixedLenFeature([wav_len], tf.float32)
+#   }
+#   record = tf.io.parse_single_example(record_proto, features=features)
+#   return record['clean'], record['mixed']
 
 
-def get_batch_inputs_from_dataset(sub_dataset_name, shuffle_records=True):
-  """
-  dataset_name: PARAM.train_name, PARAM.val_name, PARAM.test_name
-  """
-  tfrecords_list = misc_utils.datasets_dir().joinpath(sub_dataset_name, "tfrecords", "*.tfrecords")
-  files = tf.data.Dataset.list_files(str(tfrecords_list))
-  # files = files.take(FLAGS.PARAM.MAX_TFRECORD_FILES_USED)
-  if shuffle_records:
-    files = files.shuffle(PARAM.tfrecords_num_pre_set)
-  if not shuffle_records:
-    dataset = files.interleave(tf.data.TFRecordDataset,
-                               cycle_length=1,
-                               block_length=PARAM.batch_size,
-                               # num_parallel_calls=1,
-                               )
-  else:  # shuffle
-    dataset = files.interleave(tf.data.TFRecordDataset,
-                               cycle_length=8,
-                               block_length=PARAM.batch_size//8,
-                               num_parallel_calls=PARAM.n_processor_tfdata,
-                               )
-  if shuffle_records:
-    dataset = dataset.shuffle(PARAM.batch_size*10)
+# def get_batch_inputs_from_dataset(sub_dataset_name, shuffle_records=True):
+#   """
+#   dataset_name: PARAM.train_name, PARAM.val_name, PARAM.test_name
+#   """
+#   tfrecords_list = misc_utils.datasets_dir().joinpath(sub_dataset_name, "tfrecords", "*.tfrecords")
+#   files = tf.data.Dataset.list_files(str(tfrecords_list))
+#   # files = files.take(FLAGS.PARAM.MAX_TFRECORD_FILES_USED)
+#   if shuffle_records:
+#     files = files.shuffle(PARAM.tfrecords_num_pre_set)
+#   if not shuffle_records:
+#     dataset = files.interleave(tf.data.TFRecordDataset,
+#                                cycle_length=1,
+#                                block_length=PARAM.batch_size,
+#                                # num_parallel_calls=1,
+#                                )
+#   else:  # shuffle
+#     dataset = files.interleave(tf.data.TFRecordDataset,
+#                                cycle_length=8,
+#                                block_length=PARAM.batch_size//8,
+#                                num_parallel_calls=PARAM.n_processor_tfdata,
+#                                )
+#   if shuffle_records:
+#     dataset = dataset.shuffle(PARAM.batch_size*10)
 
-  dataset = dataset.map(parse_func, num_parallel_calls=PARAM.n_processor_tfdata)
-  dataset = dataset.batch(batch_size=PARAM.batch_size, drop_remainder=True)
-  # dataset = dataset.prefetch(buffer_size=PARAM.batch_size)
-  dataset_iter = tf.compat.v1.data.make_initializable_iterator(dataset)
-  clean, mixed = dataset_iter.get_next()
-  return DataSetsOutputs(initializer=dataset_iter.initializer,
-                         clean=clean,
-                         mixed=mixed)
+#   dataset = dataset.map(parse_func, num_parallel_calls=PARAM.n_processor_tfdata)
+#   dataset = dataset.batch(batch_size=PARAM.batch_size, drop_remainder=True)
+#   # dataset = dataset.prefetch(buffer_size=PARAM.batch_size)
+#   dataset_iter = tf.compat.v1.data.make_initializable_iterator(dataset)
+#   clean, mixed = dataset_iter.get_next()
+#   return DataSetsOutputs(initializer=dataset_iter.initializer,
+#                          clean=clean,
+#                          mixed=mixed)
 
 
 def _generator(noisy_list, clean_list):
